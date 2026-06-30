395c7c94b456c2b2cac25245be1f47317db9db24
hunghackgamek9-hash <hunghackgamek9@gmail.com>
2026-06-30 14:02:28 +0700

    Add files via upload

---
+++ api.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import json
+from datetime import datetime
+import os
+
+app = Flask(name)
+
+@app.post("/check")
+def check():
+    data = request.json
+    key = data["key"]
+
+    with open("license.json", "r", encoding="utf-8") as f:
+        db = json.load(f)
+
+    if key not in db:
+        return jsonify({"success": False})
+
+    info = db[key]
+
+    if info["status"] != "active":
+        return jsonify({"success": False})
+
+    expire = datetime.strptime(info["expire"], "%d/%m/%Y %H:%M:%S")
+
+    if datetime.now() > expire:
+        # Hết hạn -> xóa key
+        del db[key]
+
+        with open("license.json", "w", encoding="utf-8") as f:
+            json.dump(db, f, indent=4, ensure_ascii=False)
+
+        return jsonify({"success": False})
+
+    return jsonify({
+        "success": True,
+        "tool": info["tool"],
+        "expire": info["expire"]
+    })
+
+if name == "main":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
